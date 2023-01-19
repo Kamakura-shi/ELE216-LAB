d2717a8d39e74e2c52b5cc61589b8b3a4b2ab9cc
Kamakura-shi <116925402+Kamakura-shi@users.noreply.github.com>
2023-01-18 19:30:54 -0600

    Add files via upload

---
+++ ELE216/lab1/phase3_vscode/includes/donnees.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Genere un nombre aleatoire entre a et b */
+#define RANDBETWEEN(a,b) (a + (int)(rand() % (b - a)))
+
+unsigned int* generer_n_nombres_aleatoires(unsigned n);
---
+++ ELE216/lab1/phase3_vscode/includes/tri.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void tri_selection(unsigned int* tableau, const unsigned int taille);
