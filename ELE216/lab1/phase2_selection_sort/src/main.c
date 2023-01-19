81d3d0971a64cdb81b99bdca4e352c988b08bd5b
Kamakura-shi <116925402+Kamakura-shi@users.noreply.github.com>
2023-01-18 21:25:45 -0600

    Add files via upload

---
+++ ELE216/lab1/phase2_selection_sort/src/main.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "donnees.h"
+#include "tri.h"
+
+int main(void)
+{
+	
+	unsigned int* tableau;
+	
+	tableau = generer_n_nombres_aleatoires(10);
+
+	for (unsigned int i = 0; i < 10; i++)
+		{
+			printf("%3u ", tableau[i]);
+		}
+		printf("\n");
+
+	
+	tri_selection(tableau, 10);
+
+	for (unsigned int i = 0; i < 10; i++)
+		{
+			printf("%3u ", tableau[i]);
+		}
+		printf("\n");
+
+}
