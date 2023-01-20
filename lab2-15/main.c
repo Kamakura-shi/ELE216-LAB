bc60cdd89a708de043250991b79fe9c691f39e4c
Kamakura-shi <116925402+Kamakura-shi@users.noreply.github.com>
2023-01-20 11:40:42 -0500

    Add files via upload

---
+++ lab2-15/main.c
@@ -0,0 +1,69 @@
+/**
+ * Programme principal - main.c
+ *
+ * Par :
+ * Date : 
+ *
+ * À compléter...
+ *
+ * Exemple d'exécution:
+ *  ./a.out -t "The Matrix" -c "movie" -a "1990:2000"
+ */
+
+#include "recherche.h"
+
+int main(int argc, char *argv[]) {
+    char *titre = NULL, *genre = NULL, *annees = NULL, *categorie = NULL;
+    
+    // Lab2-HLR01 - Arguments provenant du terminal
+    // Si aucun argument n'est donné, le programme s'arrête
+    if (argc < 2) {
+        printf("Invalide! Pas assez de paramètres!\n");
+        return -1;
+        // ...
+        // À COMPLÉTER
+        // ...
+    }
+
+    // Lab2-HLR02 - Arguments possibles
+    for (int i = 1; i < argc; i++) {
+        // Option de titre
+        if (strcmp(argv[i], "-t") == 0) {
+            // On passe a l'argument qui vient après l'option
+            i++;
+            titre = argv[i];
+        }
+
+        // ...
+        // À COMPLÉTER pour les autres options possibles.
+        // ...
+    }
+
+    // Lab2-HLR03 - Argument obligatoire
+    if (titre == NULL) {
+        // ...
+        // À COMPLÉTER
+        // ...
+    }
+
+    // Création de la structure critere et stockage des arguments reçus
+    t_critere critere = creer_critere();
+
+    set_titre(critere, titre);
+    if (categorie) 
+        set_categorie(critere, categorie);
+    if (genre) 
+        set_genre(critere, genre);
+    if (annees) 
+        set_intervalle_annees(critere, annees);
+
+        
+    // ...
+    // À COMPLÉTER 
+    // ...
+    
+    // Libérer la mémoire
+    detruire_critere(critere);
+
+    return 0;
+}
