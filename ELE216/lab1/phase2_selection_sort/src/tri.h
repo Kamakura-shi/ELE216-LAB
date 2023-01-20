03163704ca7fbb5c56d4f00822ad8c5cd6eb8d5f
Kamakura-shi <116925402+Kamakura-shi@users.noreply.github.com>
2023-01-19 21:03:18 -0500

    Delete ELE216/lab1 directory

--- ELE216/lab1/phase2_selection_sort/src/tri.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define PERMUTER(a, b) { unsigned int t = a; a = b; b = t; }
-
-void tri_selection(unsigned int* tableau, const unsigned int taille);
--- ELE216/lab1/phase7_git-introduction/includes/donnees.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-/* Genere un nombre aleatoire entre a et b */
-#define RANDBETWEEN(a,b) (a + (int)(rand() % (b - a)))
-
-unsigned int* generer_n_nombres_aleatoires(unsigned n, unsigned min, unsigned max);
