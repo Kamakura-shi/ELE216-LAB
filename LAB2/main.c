1e38f2394a0521d007c423351d0ffe1286e2e3d8
Kamakura-shi <116925402+Kamakura-shi@users.noreply.github.com>
2023-02-03 12:03:15 -0500

    Add files via upload

---
+++ LAB2/main.c
@@ -0,0 +1,110 @@
+/**
+ * Programme principal - main.c
+ *
+ * Par :
+ * Date : 
+ *
+ * À compléter...
+ *
+ * Exemple d'exécution:
+ *  ./a.out -t "The Matrix" -c "movie" -a "1990:2000"
+ */
+
+#include "includes/recherche.h"
+#include "includes/resultat.h"
+#include "includes/imdb.h"
+
+int main(int argc, char *argv[]) {
+    char *titre = NULL, *genre = NULL, *annees = NULL, *categorie = NULL;
+    
+    // Lab2-HLR01 - Arguments provenant du terminal
+    // Si aucun argument n'est donné, le programme s'arrête
+    if (argc < 2) {
+        printf("Invalide! Pas assez de parametres!\n");
+        return -1;
+        // ...
+        // À COMPLÉTER
+        // ...
+    }
+
+    // Lab2-HLR02 - Arguments possibles
+    for (int i = 1; i < argc; i++) {
+        // Option de titre
+        if (strcmp(argv[i], "-t") == 0) {
+            // On passe a l'argument qui vient après l'option
+            i++;
+            titre = argv[i];
+        }
+        if (strcmp(argv[i], "-c") == 0) {
+            // On passe a l'argument qui vient après l'option
+            i++;
+            categorie = argv[i];
+        }
+        if (strcmp(argv[i], "-a") == 0) {
+            // On passe a l'argument qui vient après l'option
+            i++;
+             annees = argv[i];
+        }
+        if (strcmp(argv[i], "-g") == 0) {
+            // On passe a l'argument qui vient après l'option
+            i++;
+            genre = argv[i];
+        }
+        // ...
+        // À COMPLÉTER pour les autres options possibles.
+        // ...
+    }
+
+    // Lab2-HLR03 - Argument obligatoire
+    if (titre == NULL) {
+
+        return 0;
+        // ...
+        // À COMPLÉTER
+        // ...
+    }
+
+    // Création de la structure critere et stockage des arguments reçus
+    t_critere critere = creer_critere();
+
+    set_titre(critere, titre);
+    if (categorie) 
+        set_categorie(critere, categorie);
+    if (genre) 
+        set_genre(critere, genre);
+    if (annees) 
+        set_intervalle_annees(critere, annees);
+
+    // Lab2-HLR04 à HLR09 - Argument
+
+    // Création de la structure titre
+    t_titre un_titre = creer_titre();
+
+    // Création de la structure résultat
+    t_resultat resultat = creer_resultat;
+
+    // Ajout à la structure résultat
+    add_titre(resultat, un_titre);
+
+    // Extraction des résultats dans un fichier TSV
+    int un_resultat = t_resultat_ecrire(resultat, "title.basics.tsv");
+
+    // t_titre un_titre = get_tconst(un_titre);
+    // un_titre->tconst = "Matrix";
+
+    // ./main -t "Matrix" -c "movie" -a "1990:2000"
+
+    // Lab2-HLR15 à HLR17 - Argument
+
+    // La fonction définie au HLR09 doit écrire dans un deuxième fichier TSV tous les résultats de cotes
+    int un_resultat_cote = t_resultat_ecrire(resultat, "title.ratings.tsv");
+
+    // ...
+    // À COMPLÉTER 
+    // ...
+    
+    // Libérer la mémoire
+    detruire_critere(critere);
+
+    return 0;
+}
