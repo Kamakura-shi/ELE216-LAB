bcff681730d7a1345a4c18e64e6281b7c2ed6ccf
Kamakura-shi <116925402+Kamakura-shi@users.noreply.github.com>
2023-02-06 12:05:13 -0500

    Add files via upload

---
+++ LAB2/main.c
@@ -0,0 +1,126 @@
+/**
+ * Programme principal - main.c
+ *
+ * Par :
+ * Date : 
+ *
+ * À compléter...
+ *
+ * Exemple d'exécution:
+ *  ./a.out -t "The Matrix" -c "movie" -a "1990:2000"
+ */
+
+#include "recherche.h"
+#include "resultat.h"
+#include "imdb.h"
+
+int main(int argc, char *argv[]) {
+    char *titre = NULL, *genre = NULL, *annees = NULL, *categorie = NULL;
+    
+    // Lab2-HLR01 - Arguments provenant du terminal
+    // Si aucun argument n'est donné, le programme s'arrête
+    if (argc < 2) {
+        printf("Invalide! Pas assez de parametres!\n");
+        return -1;
+        // ...
+        // À COMPLÉTER
+        // ...
+    }
+
+    // Lab2-HLR02 - Arguments possibles
+    for (int i = 1; i < argc; i++) {
+        // Option de titre
+        if (strcmp(argv[i], "-t") == 0) {
+            // On passe a l'argument qui vient après l'option
+            i++;
+            titre = argv[i];
+        }
+        if (strcmp(argv[i], "-c") == 0) {
+            // On passe a l'argument qui vient après l'option
+            i++;
+            categorie = argv[i];
+        }
+        if (strcmp(argv[i], "-a") == 0) {
+            // On passe a l'argument qui vient après l'option
+            i++;
+             annees = argv[i];
+        }
+        if (strcmp(argv[i], "-g") == 0) {
+            // On passe a l'argument qui vient après l'option
+            i++;
+            genre = argv[i];
+        }
+        // ...
+        // À COMPLÉTER pour les autres options possibles.
+        // ...
+    }
+
+    // Lab2-HLR03 - Argument obligatoire
+    if (titre == NULL) {
+
+        return 0;
+        // ...
+        // À COMPLÉTER
+        // ...
+    }
+
+    // Création de la structure critere et stockage des arguments reçus
+    t_critere critere = creer_critere();
+
+    set_titre(critere, titre);
+    if (categorie) 
+        set_categorie(critere, categorie);
+    if (genre) 
+        set_genre(critere, genre);
+    if (annees) 
+        set_intervalle_annees(critere, annees);
+
+    // Lab2-HLR04 à HLR09
+
+    // Création de la structure titre
+    t_titre un_titre = creer_titre();
+
+    // Création de la structure résultat
+    t_resultat resultat = creer_resultat();
+
+    // Ajout à la structure résultat
+    add_titre(resultat, un_titre);
+
+    // Extraction des résultats dans un fichier TSV
+    int un_resultat = t_resultat_ecrire(resultat, "title.basics.tsv");
+
+    // ./main -t "Matrix" -c "movie" -a "1990:2000"
+
+    // Lab2-HLR17 - Visualisation des résultats - ajout de cotes
+
+    // La fonction définie au HLR09 doit écrire dans un deuxième fichier TSV tous les résultats de cotes
+    int un_resultat_cote = t_resultat_ecrire(resultat, "title.ratings.tsv");
+
+    // Lab2-HLR23 - Spécification concernant la base de données
+
+    // Les noms de la base de données doivent absolument être : title.basics.tsv et title.ratings.tsv.
+
+    // Lab2-HLR24 - Exploration de la base de données
+
+    // Le programme principal doit explorer les deux fichiers de base de données en utilisant les fonctions du module imdb
+    // get_critere(critere, "title.basics.tsv");
+    // get_cote(resultat, "title.ratings.tsv");
+    
+    // Lab2-HLR25 - Retour des résultats à l'utilisateur
+
+    // Création de la structure résultat
+    resultat = creer_resultat();
+
+    // ...
+    // À COMPLÉTER 
+    // ...
+    
+    // Lab2-HLR26 - Gestion de mémoire
+
+    // Toute mémoire allouée se doit d'être désallouée correctement avant de quitter le programme
+    
+    // Libérer la mémoire
+    detruire_critere(critere);
+
+    return 0;
+}
---
+++ LAB2/resultat.h
@@ -0,0 +1,168 @@
+/**
+ * Module resultat - resultat.h
+ *
+ * Par : Hani OUali
+ * Date : Janvier 2023
+ * Le module resultat est responsable de gérer les resultats
+ * de la recherche effectuee à l'aide de structures encapsulees de données
+ */
+
+#ifndef RESULTAT_H
+#define RESULTAT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Encapsulation de la structure titre (HLR04)
+ * Les champs sont accessibles seulement à l'intérieur du module resultat.c
+ */
+typedef struct titre* t_titre;
+
+/**
+ * Encapsulation de la structure resultat (HLR06)
+ * Les champs sont accessibles seulement à l'intérieur du module resultat.c
+ */
+typedef struct resultat* t_resultat;
+
+/**
+ * Constructeur de titre (HLR05)
+ *
+ * Cette fonction publique permet d'allouer dynamiquement une structure de titre.
+ * Elle place des valeurs par défaut dans les membres de celle-ci.
+ *
+ * Retour : pointeur vers la structure allouée
+ */
+t_titre creer_titre(void);
+
+/**
+ * Constructeur de resultat (HLR07)
+ *
+ * Cette fonction publique permet d'allouer dynamiquement une structure de resultat.
+ * Elle place des valeurs par défaut dans les membres de celle-ci.
+ *
+ * Retour : pointeur vers la structure allouée
+ */
+t_resultat creer_resultat(void);
+
+
+/* MUTATEURS DES CRITÈRES */
+
+/**
+ * Mutateur de titre (HLR08)
+ *
+ * Permet d'ajouter une donnée de titre à la structure resultat.
+ *
+ * resultat : la structure à modifier
+ * titre : chaine de caractères constituant le titre à ajouter
+ */
+void add_titre(t_resultat resultat, t_titre titre);
+
+/**
+ * Mutateur de titre (HLR15)
+ *
+ * Permet d'ajouter une donnée de titre à la structure titre.
+ *
+ * resultat : la structure à modifier
+ * titre : chaine de caractères constituant le titre à ajouter
+ */
+void add_cote(t_titre titre, int averageRating, int numVotes);
+
+
+/* OBSERVATEURS DES TITRES */
+
+/**
+ * Observateur de constante d'identification
+ *
+ * Permet d'obtenir la constante d'identification de la structure titre
+ *
+ * titre : la structure à observer
+ *
+ * Retour : chaine de caractères constituant la constante d'identification de la structure donnée
+ *          en paramètre
+ */
+char* get_tconst(t_titre titre);
+
+/**
+ * Observateur de titre principal
+ *
+ * Permet d'obtenir le titre principal de la structure titre
+ *
+ * titre : la structure à observer
+ *
+ * Retour : chaine de caractères constituant le titre principal de la structure donnée
+ *          en paramètre
+ */
+char* get_primaryTitle(t_titre titre);
+
+/**
+ * Observateur de d'année de début
+ *
+ * Permet d'obtenir l'année de début de la structure titre
+ *
+ * titre : la structure à observer
+ *
+ * Retour : l'année constituant le début de l'intervalle de la structure donnée
+ *          en paramètre
+ */
+int get_startYear(t_titre titre);
+
+/**
+ * Observateurs de genres
+ *
+ * Permet d'obtenir les genres de la structure titre
+ *
+ * titre : la structure à observer
+ *
+ * Retour : chaine de caractères constituant les genres de la structure donnée
+ *          en paramètre
+ */
+char* get_genres(t_titre titre);
+
+/**
+ * Observateurs de type de titre
+ *
+ * Permet d'obtenir le type de titre de la structure titre
+ *
+ * titre : la structure à observer
+ *
+ * Retour : chaine de caractères constituant le type de titre de la structure donnée
+ *          en paramètre
+ */
+char* get_titleType(t_titre titre);
+
+/**
+ * Observateur de note moyenne
+ *
+ * Permet d'obtenir la note moyenne de la structure titre
+ *
+ * titre : la structure à observer
+ *
+ * Retour : la note moyenne de la structure donnée
+ *          en paramètre
+ */
+int get_averageRating(t_titre titre);
+
+/**
+ * Observateur de nombre de votes
+ *
+ * Permet d'obtenir le nombre de votes de la structure titre
+ *
+ * titre : la structure à observer
+ *
+ * Retour : le nombre de votes de la structure donnée
+ *          en paramètre
+ */
+int get_numVotes(t_titre titre);
+
+
+/**
+/* Permet d'écrire dans un fichier TSV tous les résultats (HLR09)
+*
+* resultat_ptr : la structure à visualiser
+* nom_fichier : fichier à ecrire
+*/
+int t_resultat_ecrire(t_resultat resultat_ptr, const char* nom_fichier);
+
+#endif
